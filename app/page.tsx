ec5f34d02702c7c3ce749a1a94b8f25b72e9b96f
damin <34189426+damil27@users.noreply.github.com>
2024-05-19 13:20:53 +0100

    calling testing backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import ProductCard from "./Components/ProductCard";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="">
       <h1>Hello there</h1>
+      <Link href="/users" >User page</Link>
+      <ProductCard />
     </main>
   );
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const UserPage = () => {
-  return <div>UserPage</div>;
+
+interface User{
+  id: number;
+  name: string;
+}
+const UserPage = async() => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const user: User[] = await res.json()
+
+  return <div>
+    <h1>User Details here</h1>
+    <ul>
+      {user.map(u => <li key={u.id}>{ u.name}</li>)}
+    </ul>
+  </div>;
 };
 
 export default UserPage;
