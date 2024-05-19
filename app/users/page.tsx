3a82286f8d3915eb7eae42645b5f77e18b724b69
damin <34189426+damil27@users.noreply.github.com>
2024-05-19 15:46:02 +0100

    introduction of daisy Ui to enhance tailwind styling

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,20 +1,35 @@
 import React from "react";
 
-
-interface User{
+interface User {
   id: number;
   name: string;
+  email: string;
 }
-const UserPage = async() => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users')
-  const user: User[] = await res.json()
+const UserPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const user: User[] = await res.json();
 
-  return <div>
-    <h1>User Details here</h1>
-    <ul>
-      {user.map(u => <li key={u.id}>{ u.name}</li>)}
-    </ul>
-  </div>;
+  return (
+    <div>
+      <h1>User Details here</h1>
+      <table className="table table-border">
+        <thead>
+          <tr>
+            <th>User Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {user.map((u) => (
+            <tr key={u.id}>
+              <td>{u.name}</td>
+              <td>{u.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default UserPage;
